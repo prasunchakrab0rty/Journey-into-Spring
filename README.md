799bbad6e934d28291d4edf5ec87370558780fdf
Prasun Chakraborty <lordprasun@gmail.com>
2020-02-24 09:27:21 +0530

    Cleaning up the master

---
+++ README.md
@@ -0,0 +1,4 @@
+# List of Spring Projects in this repository
+
+#### Spring Framework: Core Spring
+* https://github.com/prasunchakrab0rty/Journey-into-Spring/tree/creating_your_first_spring_boot_application
--- src/main/java/com/prasun/startspring/repositories/SessionRep.java
+++
@@ -1,7 +0,0 @@
-package com.prasun.startspring.repositories;
-
-import com.prasun.startspring.models.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRep extends JpaRepository<Session, Long> {
-}
