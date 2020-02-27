a0d448298da2834e011b508ef117d0db6323a53c
Prasun Chakraborty <lordprasun@gmail.com>
2020-02-27 11:25:29 +0530

    Constructor Injection

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -9,8 +9,8 @@ import service.SpeakerServiceImpl;
 public class AppConfig {
     @Bean(name = "speakerService")
     public SpeakerService getSpeakerService(){
-        SpeakerServiceImpl service = new SpeakerServiceImpl();
-        service.setRepository(getSpeakerRepository());
+        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+//        service.setRepository(getSpeakerRepository());
         return service;
     }
 
